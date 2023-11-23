c6dd5bb3864ee11ca7a51af4a9248ea3da32ea0a
Mohammad Karbalaee <muhammad.ksht@gmail.com>
2023-11-23 04:46:46 +0330

    data persistence is implemented using the local storage of the browser

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskPage from './screens/TaskPage';
 import { Route, Routes } from 'react-router';
 import EditPage from './screens/EditPage';
 import { Task } from './models/Task';
 
+// Define a type for the tasks array
+type TasksType = Task[];
+
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const storedTasks = localStorage.getItem('tasks');
+  const initialTasks: TasksType = storedTasks ? JSON.parse(storedTasks) : [];
+  const [tasks, setTasks] = useState<TasksType>(initialTasks);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <Routes>
